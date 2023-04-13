b5baccc9ff5a9722d9c41bd5669b45cc625262bb
Kaden Kapadia <106453833+kkapadia10@users.noreply.github.com>
2023-04-13 12:18:43 -0700

    Final edits

--- registrations.sql
+++ registrations.sql
@@ -4,13 +4,13 @@ CREATE TABLE IF NOT EXISTS student_courses (
   grade FLOAT DEFAULT NULL
 );
 
-INSERT INTO student_courses (studentid, course) VALUES 
-(1, 'INFO330A'),
-(1, 'INFO448A'),
- (1, 'INFO314'),
- (3, 'INFO330A'),
- (3, 'INFO449A'),
- (2, 'BAW0100'),
- (2, 'BAW100A'),
-  (4, 'BAW0100');
+INSERT INTO student_courses (studentid, course, grade) VALUES 
+(1, 'INFO330A', 3.4),
+(1, 'INFO448A', 3.8),
+ (1, 'INFO314', 2.9),
+ (3, 'INFO330A', 4.0),
+ (3, 'INFO449A', 3.5),
+ (2, 'BAW0100', 1.7),
+ (2, 'BAW100A', 3.9),
+  (4, 'BAW0100', 3.3);
   
